39af722b7133b2b159edfcdbee04c1845a5da363
Admin <Admin@Admin-PC>
2020-12-02 16:43:11 +0530

    First Assignment of Rest-Assured-BDD-Framework

---
+++ Rest_Assured_Assignments/src/test/java/stepDefinition/Response_Script_Assignment.java
@@ -0,0 +1,37 @@
+package stepDefinition;
+
+import org.testng.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Response_Script_Assignment 
+{
+	Response re;
+	
+	@Given("API for foreign exchange")
+	public void api_for_foreign_exchange()
+	{
+		RestAssured.baseURI="https://api.ratesapi.io/";
+		
+	}
+	@When("posted with correct information")
+	public void posted_with_correct_information() {
+		re = RestAssured.get("/api/latest");
+		
+	}
+
+	@Then("validate positive response code received")
+	public void validate_positive_response_code_received() 
+	{
+		int statusCode = re.getStatusCode();
+		System.out.println("Status code is "+statusCode);
+	
+		System.out.println("Status Code is: " + re.getStatusCode());
+		Assert.assertEquals(200, re.getStatusCode());
+		
+	}
+}
