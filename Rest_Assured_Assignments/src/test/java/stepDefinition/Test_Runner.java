76fe42ad401da07fa7d7c97f2652098a1a0c0650
Admin <Admin@Admin-PC>
2020-12-07 15:05:40 +0530

    revised

--- Rest_Assured_Assignments/src/test/java/stepDefinition/Test_Runner.java
+++ Rest_Assured_Assignments/src/test/java/stepDefinition/Test_Runner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\features", glue = {"stepDefinition"})
+@CucumberOptions(features = "src\\test\\resources\\features\\AcceptanceCriteria 1.feature", glue = {"stepDefinition"})
 
 public class Test_Runner 
 {
